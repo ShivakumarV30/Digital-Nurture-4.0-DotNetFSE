0900a0e43e1385816bad8bfd0022d0034898c82d
ShivakumarV30 <159005355+ShivakumarV30@users.noreply.github.com>
2025-06-22 23:12:19 +0530

    Add files via upload

---
+++ factorymethodpatter/factorymethodpattern/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace FactoryMethodPatternExample
+{
+    public interface IDocument
+    {
+        void Open();
+    }
+
+    public class WordDocument : IDocument
+    {
+        public void Open()
+        {
+            Console.WriteLine("Opening Word Document.");
+        }
+    }
+
+    public class PdfDocument : IDocument
+    {
+        public void Open()
+        {
+            Console.WriteLine("Opening PDF Document.");
+        }
+    }
+
+    public class ExcelDocument : IDocument
+    {
+        public void Open()
+        {
+            Console.WriteLine("Opening Excel Document.");
+        }
+    }
+
+    public abstract class DocumentFactory
+    {
+        public abstract IDocument CreateDocument();
+    }
+
+    public class WordDocumentFactory : DocumentFactory
+    {
+        public override IDocument CreateDocument()
+        {
+            return new WordDocument();
+        }
+    }
+
+    public class PdfDocumentFactory : DocumentFactory
+    {
+        public override IDocument CreateDocument()
+        {
+            return new PdfDocument();
+        }
+    }
+
+    public class ExcelDocumentFactory : DocumentFactory
+    {
+        public override IDocument CreateDocument()
+        {
+            return new ExcelDocument();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DocumentFactory wordFactory = new WordDocumentFactory();
+            IDocument word = wordFactory.CreateDocument();
+            word.Open();
+
+            DocumentFactory pdfFactory = new PdfDocumentFactory();
+            IDocument pdf = pdfFactory.CreateDocument();
+            pdf.Open();
+
+            DocumentFactory excelFactory = new ExcelDocumentFactory();
+            IDocument excel = excelFactory.CreateDocument();
+            excel.Open();
+        }
+    }
+}
