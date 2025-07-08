bf364137a6da61ecc9c52583495dc4e7e7ece4cf
ShivakumarV30 <shivakumarkv.2004@gmail.com>
2025-07-08 19:20:11 +0530

    week 3

---
+++ RetailInventory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RetailInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Retail Inventory System - ORM Setup Complete");
+        }
+    }
+}
